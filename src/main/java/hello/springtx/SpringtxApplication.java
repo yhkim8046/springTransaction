4d6d47664dc43792527925dd2d046eaba4d9c744
Yuhwan Kim <yhkim8046@gmail.com>
2024-03-24 13:01:32 +1300

    Initial commit

---
+++ src/main/java/hello/springtx/SpringtxApplication.java
@@ -0,0 +1,14 @@
+package hello.springtx;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtxApplication.class, args);
+	}
+
+}
